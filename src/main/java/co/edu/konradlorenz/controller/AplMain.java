71c3bfcad46c5dc0db13d594452336a10f69e9a3
ParraDav <df.parra.07@gmail.com>
2025-02-26 19:11:13 -0500

    Inicio

---
+++ src/main/java/co/edu/konradlorenz/controller/AplMain.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package co.edu.konradlorenz.controller;
+
+/**
+ *
+ * @author dfpar
+ */
+public class AplMain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
